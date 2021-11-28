83bb63de586d393861f6addf3feb6b53a0ddf9e0
Davis <davis.phan-tran.1@ens.etsmtl.ca>
2021-11-28 10:16:02 -0500

    Save Current version

--- Scripts/Wave_controller.gd
+++ Scripts/Wave_controller.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+
+
+var enemy_One = preload("res://Scene/Enemy/Enemy_one.tscn")
+var enemy_Two = preload("res://Scene/Enemy/Enemy_two.tscn")
+var enemy_Three = preload("res://Scene/Enemy/Enemy_three.tscn")
+
 #what wave we are on and sub wave each enemy will spawn from and the max limits of wave
 var wave = 0
 var sub_wave = 0 
@@ -8,8 +14,8 @@ export (int) var wave_limit
 #export (Array, NodePath) var spawn_position
 
 #list of all the enemies with spawn rates
-var enemies = [] #differente scene enemy
-var enemy_rate = [0.25, 0.47]
+var enemies = [enemy_One, enemy_Two, enemy_Three] #differente scene enemy
+var enemy_rate = [0.25, 0.50, 1.0]
 
 # Nb of enemies in a sub wave
 export(Array, Array, int) var enemy_count
@@ -35,7 +41,7 @@ func spawn_wave():
 
 func _on_Spawn_Rate_timeout():
 	if enemy_amount>= 1: #Spawn enemy if enemy amount still higher than 0
-		#spawn_enemy()
+		spawn_enemy()
 		enemy_amount -= 1
 	elif enemy_amount <= 0 :#if not higher than 0
 		sub_wave += 1 #Increase subwave
